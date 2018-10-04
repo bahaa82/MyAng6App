7e475f791eac4070e77a4a38bfd5335101871e4f
bahaa82 <bahaa1982@gmail.com>
2018-10-04 13:34:07 +0300

    first commet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProductListComponent }  from './products/product-list.component';
+import { ProductFilterPipe }  from './products/product-filter.pipe';
+import { StarComponent }  from './shared/star.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,ProductListComponent,ProductFilterPipe,StarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/star.component.ts
@@ -0,0 +1,28 @@
+import {Component,OnChanges,Input,Output,EventEmitter} from '@angular/core';
+
+
+@Component({
+  selector:'ai-star',
+  templateUrl:'star.component.html',
+  styleUrls:['star.component.css']
+})
+
+export class StarComponent implements OnChanges {
+       @Input() rating : number ;
+        starWidth:number;
+       @Output() ratingClicked : EventEmitter<string> =  new EventEmitter<string>();
+
+    ngOnChanges(): void {
+        this.starWidth = this.rating*86/5;
+    }
+
+    onCLick(): void {
+        this.ratingClicked.emit(`The rating ${this.rating} was clicked !`);
+    }
+
+    starOnCLick(): void {
+      this.ratingClicked.emit(`The rating ${this.rating} was clicked !`);
+  }
+
+
+}
