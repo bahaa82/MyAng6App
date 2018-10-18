6913582ca5d855fee0a320284c5dc39913f8ba8f
bahaa82 <bahaa1982@gmail.com>
2018-10-18 23:20:33 +0300

    re module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { ProductListComponent }  from './products/product-list.component';
-import { ProductFilterPipe }  from './products/product-filter.pipe';
-import { StarComponent }  from './shared/star.component';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {RouterModule} from '@angular/router';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
-import { ProductDetailGuard } from './products/product-detail.guard';
-import { ProductModule } from './producs/product.module';
+import { ProductModule } from './products/product.module';
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ProductFilterPipe,
-    StarComponent,
-     ProductDetailComponent,
+
      WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id',canActivate:[ProductDetailGuard], component: ProductDetailComponent },
+
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { StarComponent } from '../shared/star.component';
+import { FormsModule } from '@angular/forms';
+import { ProductFilterPipe } from './product-filter.pipe';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './product-detail.guard';
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id',canActivate:[ProductDetailGuard], component: ProductDetailComponent },
+    ])
+
   ],
-  declarations: []
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    StarComponent,
+    ProductFilterPipe
+  ]
 })
 export class ProductModule { }
